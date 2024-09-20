4b19c3072b099047e97430f22a9a5b2048d7a87f
KHALED AHAMED.S <98593343+khaledahamed8080@users.noreply.github.com>
2024-09-20 22:32:30 +0530

    Day 1 practice

---
+++ demo.java
@@ -0,0 +1,103 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) throws IOException {
+        
+        // System.out.println("print this line \nin the new line ");
+        // System.out.println("\tthis line will have gap in the begining");
+        // System.out.println("this line will have a \\ slash printed with it");
+        // System.out.println("this line will have an    \bbackslash");
+
+        // int a=10;
+        // int b=3;
+
+        //  int mod=a%b;
+        // System.out.println(mod);
+        
+        // int a=5;
+        // int b=10;
+        // int c=4;
+
+        // if (a>b){
+        //     if(a>c){
+        //         System.out.println("a is greater");
+        //     }else{
+        //         System.out.println("c is greater");
+        //     }
+        // }else{
+        //     if(b>c){
+        //         System.out.println("b is greater");
+
+        //     }else{
+        //         System.out.println("c is greater");
+        //     }
+        // }
+
+        // int basic=60000;
+        // double bonus,gross;
+
+        // if (basic<10000){
+        //     bonus=0.75*basic;
+        //     gross=bonus+basic;
+        //     System.out.println("The total pay is:"+gross);
+        // }else if(basic>10000 && basic<=20000){
+        //     bonus=0.5*basic;
+        //     gross=bonus+basic;
+        //     System.out.println("The total pay is:"+gross);
+        // }else if(basic>20000 && basic<=50000){
+        //     bonus=0.25*basic;
+        //     gross=bonus+basic;
+        //     System.out.println("The total pay is:"+gross);
+        // }else{
+        //     System.out.println("No bonus jus have the fucking salary itself and shut ur ass");
+        // }
+        
+        // int a=5;
+        // int c=0;
+        // while(c<=10){
+        //     a*=2;
+        //     System.out.println(a);
+            
+        //     c++;
+        // }
+
+        // Scanner scanner=new Scanner(System.in);
+
+        // System.out.print("ENTER YOUR LETTER :");
+        // char num=scanner.next().charAt(0);
+        // System.out.println("THE LETTER U HAVE ENTERED IS:"+num);
+        // char choice;
+        // System.out.println("PLEASE ENTER A FOOD NUMBER OF YOUR CHOICE:\n1.A\n2.B\n3.C\n4.None\nPLEASE ENTER YOUR CHOICE");
+        
+        // choice=(char)System.in.read();
+        // switch(choice){
+        //     case '1':System.out.println("THE FOOD YOU HAVE ENTERED IS:A");
+        //     break;
+        //     case '2':System.out.println("THE FOOD YOU HAVE ENTERED IS:B");
+        //     break;
+        //     case '3':System.out.println("THE FOOD YOU HAVE ENTERED IS:C");
+        //     break;
+        //     case '4':System.out.println("NO FOOD IS NEEDED");
+        //     break;
+        // }
+        // for (int i=0;i<=30;i++){
+        //     if(i%10==0){
+        //         System.out.println("it is divisible by 10="+i);
+        //         continue;
+        //     }
+        //     else{
+        //         System.out.println(i);
+        //     }
+        // }
+
+
+        char a,b,c;
+        a='y';
+        b=3;
+        c='S';
+        System.out.println("the character:"+a+" is a uppercase "+Character.toString(a));
+
+        }
+}
+ 
