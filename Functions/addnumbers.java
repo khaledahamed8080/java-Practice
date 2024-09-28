821bbb0e69c27636a50d763281e0efe2bb8f1361
KHALED AHAMED.S <98593343+khaledahamed8080@users.noreply.github.com>
2024-09-28 16:58:43 +0530

    .

---
+++ Functions/addnumbers.java
@@ -0,0 +1,16 @@
+package Functions;
+//Enter 3 numbers from the user & make a function to print their average.
+
+class Maths{
+    int sum;
+    public void add(int a,int b,int c){
+     sum=(a+b+c)/3;
+     System.out.println("THE AVERAGE IS:"+sum);
+    }
+}
+public class addnumbers {
+    public static void main(String[] args) {
+        Maths m=new Maths();
+        m.add(3,7,5);
+    } 
+}
