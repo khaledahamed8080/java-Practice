76a31869a7d8ca960f41852e3111918079163639
KHALED AHAMED.S <98593343+khaledahamed8080@users.noreply.github.com>
2024-09-28 10:40:50 +0530

    Basic patterns

---
+++ Pattern/EvenOddHalfPyramid.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+public class EvenOddHalfPyramid {
+    public static void main(String[] args) {
+        int n=5;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                sum=i+j;
+                if(sum%2==0){
+                    System.err.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
